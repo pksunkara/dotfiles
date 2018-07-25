6312dcb0320dbd5500c6027f67734ed383dbd521
Pavan Kumar Sunkara <pavan.sss1991@gmail.com>
2018-07-25 03:39:01 +0200

    Have status for empty git repos

--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -9,12 +9,15 @@ function fish_prompt
 	set -l folder (set_color cyan)(pwd | sed "s|$HOME|⌂|" | sed "s|⌂/Coding|⍴|")" "
 
 	set -l is_git_repo (command git rev-parse --is-inside-work-tree ^/dev/null)
+	set -l have_commits (command git log --oneline -n 1 ^/dev/null)
 	set -l branch ""
 	set -l git_dirty ""
 	set -l git_stash ""
 	set -l git_local ""
 
-	if test -n "$is_git_repo"
+	if test -n "$is_git_repo" -a -z "$have_commits"
+		set branch (set_color red)"(empty) "
+	else if test -n "$is_git_repo"
 		set git_branch_name (command git symbolic-ref --short HEAD ^/dev/null; or command git show-ref --head -s --abbrev | head -n1 ^/dev/null)
 		set branch (set_color yellow)$git_branch_name
 
