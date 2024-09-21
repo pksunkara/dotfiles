319c5d032300b1e7402e9a13670d0ed2f59e63e3
Pavan Kumar Sunkara <pavan.sss1991@gmail.com>
2024-09-22 00:01:11 +0100

    Added lspconfig

---
+++ nvim/lua/plugin_settings/lspconfig.lua
@@ -0,0 +1,12 @@
+local wk = require('which-key')
+local lspc = require('lspconfig')
+
+lspc.rust_analyzer.setup({})
+
+require('typescript-tools').setup({
+  expose_as_code_action = 'all',
+})
+
+wk.add({
+  { '<C-.>', '<D-.>', vim.lsp.buf.code_action, desc = "Code actions", mode = "nixsotc" },
+})
